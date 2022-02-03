8fb97b27b26473ae675f1dbd484c648bfeefff6f
Isak Solheim <42111513+isaksolheim@users.noreply.github.com>
2022-02-03 20:22:02 +0100

    Add tailwind (#1)

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -1,32 +1,39 @@
 require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV || 'development'}`
-})
+  path: `.env.${process.env.NODE_ENV || 'development'}`,
+});
 
-const clientConfig = require('./client-config')
+const clientConfig = require('./client-config');
 
-const isProd = process.env.NODE_ENV === 'production'
-
-console.log(process.env.NODE_ENV);
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   siteMetadata: {
-      title: `Orbit NTNU`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Orbit NTNU`,
+    description: `Student organization building satellites.`,
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [{
-    resolve: 'gatsby-source-sanity',
-    options: {
-      ...clientConfig.sanity,
-      token: process.env.SANITY_READ_TOKEN,
-      watchMode: !isProd,
-      overlayDrafts: !isProd
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-postcss',
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        ...clientConfig.sanity,
+        token: process.env.SANITY_READ_TOKEN,
+        watchMode: !isProd,
+        overlayDrafts: !isProd,
+      },
     },
-    __key: "images"
-  }]
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+  ],
 };
