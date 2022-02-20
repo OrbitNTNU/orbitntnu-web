573bf0af9431b2cc139972467e72c323a95eb8f5
isaksolheim <isak.m.h.s@gmail.com>
2022-02-20 16:10:56 +0100

    Add AboutUsBanner component

---
+++ frontend/src/components/AboutUsBanner/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+
+export const AboutUsBanner = () => (
+  <StaticImage src="../../images/space.jpg" className="h-screen" />
+);
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,10 +2,12 @@ import * as React from "react";
 import { graphql } from "gatsby";
 import { Layout } from "../templates/Layout";
 import { LandingHero } from "../components/LandingHero";
+import { AboutUsBanner } from "../components/AboutUsBanner";
 
 const IndexPage = ({ data, errors }) => (
   <Layout>
     <LandingHero />
+    <AboutUsBanner />
     {/**
     {data.categories.edges.map((category, _id) => (
       <p key={_id}>{category.node.title}</p>
