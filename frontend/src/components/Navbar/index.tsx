6f42644f5319867af0a155bcfa0104cbdbd76bc6
isaksolheim <isak.m.h.s@gmail.com>
2022-02-20 15:24:04 +0100

    Update navbar ui

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import { StaticImage } from 'gatsby-plugin-image';
-import { OpenNav } from './OpenNav';
-import { FaBars } from '@react-icons/all-files/fa/FaBars';
-import { Link } from 'gatsby';
+import React, { useState } from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import { OpenNav } from "./OpenNav";
+import { FaBars } from "@react-icons/all-files/fa/FaBars";
+import { Link } from "gatsby";
 
 export const Navbar = () => {
   const [toggle, setToggle] = useState(false);
@@ -10,22 +10,36 @@ export const Navbar = () => {
   const handleToggle = () => setToggle(!toggle);
 
   return (
-    <nav className='bg-black text-white p-4'>
-      <Link to='/'>
+    <nav className="text-white p-4 relative m-4">
+      <div className="bg-black opacity-80 w-full h-full absolute top-0 left-0 z-0" />
+
+      <Link to="/">
         <StaticImage
-          src='../../images/orbitimage.png'
-          alt='Orbit'
-          className='w-32'
-          placeholder='none'
+          src="../../images/orbitimage.png"
+          alt="Orbit"
+          className="w-32"
+          placeholder="none"
         />
       </Link>
+
+      <ul className="text-white absolute top-7 left-44 text-lg">
+        <li className="inline-block mr-4">
+          <Link to="/selfiesat">SelfieSat</Link>
+        </li>
+        <li className="inline-block mr-4">
+          <Link to="/selfiesat">FRAMSAT-1</Link>
+        </li>
+        <li className="inline-block mr-4">
+          <Link to="/selfiesat">Sub-Orbital</Link>
+        </li>
+      </ul>
 
       {toggle ? (
         <OpenNav handleToggle={handleToggle} />
       ) : (
         <FaBars
           onClick={handleToggle}
-          className='absolute right-4 top-8 cursor-pointer'
+          className="absolute right-4 top-8 cursor-pointer"
         />
       )}
     </nav>
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,17 +1,14 @@
-import * as React from 'react';
-import { graphql } from 'gatsby';
-import { Button } from '../components/Button';
-import { Navbar } from '../components/Navbar';
-import { Footer } from '../components/Footer';
-import { Layout } from '../templates/Layout';
+import * as React from "react";
+import { graphql } from "gatsby";
+import { Layout } from "../templates/Layout";
 
 const IndexPage = ({ data, errors }) => (
   <Layout>
-    <Button label='Knapp' />
-
+    {/**
     {data.categories.edges.map((category, _id) => (
       <p key={_id}>{category.node.title}</p>
     ))}
+     */}
   </Layout>
 );
 
