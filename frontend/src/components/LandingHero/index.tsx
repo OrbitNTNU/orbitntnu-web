8090fb5ffcb41f1863b1fce440ce598b5ba53a50
isaksolheim <isak.m.h.s@gmail.com>
2022-02-26 14:24:29 +0100

    Update landing hero

--- frontend/src/components/LandingHero/index.tsx
+++ frontend/src/components/LandingHero/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import OrbitCompilation from "../../images/orbit-compilation.mp4";
+import OrbitCompilation from "../../images/tmpvideo.mp4";
 
 export const LandingHero = () => (
   <div className="flex justify-center">
-    <video autoPlay loop className="h-screen">
+    <video autoPlay loop className="w-screen">
       <source src={OrbitCompilation} type="video/mp4" />
     </video>
     <h1 className="absolute top-44 left-32 text-white text-6xl font-bold max-w-lg">
-      Hop On Our Jorney to Outer Space!
+      HOP ON OUR JOURNEY TO OUTER SPACE!
     </h1>
   </div>
 );
