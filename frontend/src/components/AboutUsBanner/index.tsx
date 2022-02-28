7fe4d925622843ab23a31e5ff1826c24358fccc0
isaksolheim <isak.m.h.s@gmail.com>
2022-02-28 11:28:28 +0100

    Add about us section

--- frontend/src/components/AboutUsBanner/index.tsx
+++ frontend/src/components/AboutUsBanner/index.tsx
@@ -1,6 +1,19 @@
 import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
+import { Button } from "../Button";
 
 export const AboutUsBanner = () => (
-  <StaticImage src="../../images/space.jpg" className="h-screen" />
+  <section className="relative flex justify-center">
+    <StaticImage src="../../images/earth.jpg" className="w-3/5" />
+
+    <div className="absolute top-64 left-100 w-64">
+      <h2 className="text-4xl text-white">Orbit NTNU</h2>
+      <p className="text-white mb-2">
+        We are designing and building small satellites that will be launched
+        into space. We want to create the next generation of space engineers by
+        working on complex space projects.
+      </p>
+      <Button label="LEARN MORE" />
+    </div>
+  </section>
 );
--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 interface ButtonProps {
   label: string;
 }
 
 export const Button = ({ label }: ButtonProps) => (
-  <button className='bg-blue-50'>{label}</button>
+  <button className="bg-blue-600 text-white py-2 px-4">{label}</button>
 );
