23599a691befca31c4a85bbbfde70cacc50b5386
isaksolheim <isak.m.h.s@gmail.com>
2022-03-05 17:39:04 +0100

    Make site blank

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,16 +1,19 @@
-import * as React from "react";
-import { graphql } from "gatsby";
+import React from "react";
 import { Layout } from "../templates/Layout";
 import { LandingHero } from "../components/LandingHero";
 import { AboutUsBanner } from "../components/AboutUsBanner";
 import { BannerLinkList } from "../components/BannerLinkList";
 
-const IndexPage = ({ data, errors }) => (
+const IndexPage = () => {
+  /*
   <Layout>
     <LandingHero />
     <AboutUsBanner />
     <BannerLinkList />
   </Layout>
-);
+  */
+
+  return null;
+};
 
 export default IndexPage;
