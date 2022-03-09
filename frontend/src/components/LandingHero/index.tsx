500fd7b89969c3c110aa5cba6119a31c9880d213
isaksolheim <isak.m.h.s@gmail.com>
2022-03-09 13:14:39 +0100

    Add desktop friendly LandingHero

--- frontend/src/components/LandingHero/index.tsx
+++ frontend/src/components/LandingHero/index.tsx
@@ -19,7 +19,7 @@ export const LandingHero = ({ topText, mobileImage }: LandingHeroProps) => {
   return (
     <div className="relative">
       {width > 640 ? (
-        <video autoPlay loop className="w-screen">
+        <video autoPlay loop className="w-screen opacity-80">
           <source src={OrbitCompilation} type="video/mp4" />
         </video>
       ) : (
@@ -28,7 +28,7 @@ export const LandingHero = ({ topText, mobileImage }: LandingHeroProps) => {
           alt="Engineers Working"
         />
       )}
-      <h1 className="absolute top-4 left-4 text-3xl font-bold max-w-64">
+      <h1 className="absolute top-4 left-4 text-3xl p-2 bg-gray-900 bg-opacity-50 font-black uppercase max-w-64 md:top-16 md:left-1/4 md:-ml-32 md:max-w-md md:text-6xl">
         {topText}
       </h1>
     </div>
