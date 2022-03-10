50b942e961388c7d1522fd60ebd55fc12eb6a050
Isak Solheim <42111513+isaksolheim@users.noreply.github.com>
2022-03-10 12:03:28 +0100

    Add header to contact page (#19)
    * Decrease header bottom margin
    * Remove unused import
    * Add header to contact page

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Header } from "../components/Header";
-import { Navbar } from "../components/Navbar";
 import { Layout } from "../templates/Layout";
 
 const AboutUs = () => (
