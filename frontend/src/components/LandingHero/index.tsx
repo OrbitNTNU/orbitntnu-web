f37e099350499bd34d979300f928edfc082dbf29
isaksolheim <isak.m.h.s@gmail.com>
2022-03-10 16:17:23 +0100

    Fix video autoplay bug

--- frontend/src/components/LandingHero/index.tsx
+++ frontend/src/components/LandingHero/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { IGatsbyImageData } from "gatsby-plugin-image";
-import OrbitCompilation from "../../images/tmpvideo.mp4";
+import OrbitCompilationMp4 from "../../images/tmpvideo.mp4";
+import OrbitCompilationOgv from "../../images/tmpvideo.ogv";
+import PlaceholderImage from "../../images/mobile-hero.png";
 import { useWindowSize } from "../../utils/hooks/useWindowSize";
 import { GatsbyImage } from "gatsby-plugin-image";
 
@@ -19,8 +21,15 @@ export const LandingHero = ({ topText, mobileImage }: LandingHeroProps) => {
   return (
     <div className="relative" data-scroll-section>
       {width > 640 ? (
-        <video autoPlay loop className="w-screen opacity-70">
-          <source src={OrbitCompilation} type="video/mp4" />
+        <video
+          autoPlay
+          loop
+          muted
+          className="w-screen opacity-70"
+          poster={PlaceholderImage}
+        >
+          <source src={OrbitCompilationMp4} type="video/mp4" />
+          <source src={OrbitCompilationOgv} type="video/ogg" />
         </video>
       ) : (
         <GatsbyImage
@@ -29,14 +38,6 @@ export const LandingHero = ({ topText, mobileImage }: LandingHeroProps) => {
           className="opacity-70"
         />
       )}
-      {/*
-      <h1
-        className="absolute top-16 left-4 text-3xl p-2 font-black uppercase max-w-64 
-        md:top-32 md:left-1/4 md:-ml-32 md:max-w-md md:text-6xl"
-      >
-        {topText}
-      </h1>
-       */}
     </div>
   );
 };
