2981b57f6f6aaefb0d588bac8c9578a84548fb87
isaksolheim <isak.m.h.s@gmail.com>
2022-03-10 20:51:24 +0100

    Add about us mobile page

---
+++ frontend/src/components/FadeInSection/index.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./FadeInSection.css";
+
+export const FadeInSection: React.FC = ({ children }) => {
+  const [isVisible, setVisible] = useState(false);
+  const domRef = useRef();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(
+        (entry) => entry.isIntersecting && setVisible(entry.isIntersecting)
+      );
+    });
+    observer.observe(domRef.current);
+    return () => observer.unobserve(domRef.current);
+  }, []);
+
+  return (
+    <div
+      className={`fade-in-section ${isVisible ? "is-visible" : ""}`}
+      ref={domRef}
+    >
+      {children}
+    </div>
+  );
+};
--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -1,4 +1,6 @@
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
+import { FadeInSection } from "../components/FadeInSection";
 import { Header } from "../components/Header";
 import { Layout } from "../templates/Layout";
 
@@ -11,6 +13,53 @@ const AboutUs = () => (
       We are very grateful for the support we receive, and are always looking for new companies 
       to work with. Want to be a part of our journey? Send an email to cmo@orbitntnu.com!"
     />
+    <section className="mt-24 px-8 relative" data-scroll-section>
+      <h2 className="text-2xl">Student Organization Building Satellites</h2>
+      <p>
+        Orbit is run with both technical and non-technical support from our
+        sponsors. We are very grateful for the support we receive, and are
+        always looking for new companies to work with. Want to be a part of our
+        journey? Send an email to cmo@orbitntnu.com!
+      </p>
+      <FadeInSection>
+        <StaticImage
+          src="../images/selfiesat.png"
+          alt="SelfieSat"
+          className="mb-8"
+        />
+      </FadeInSection>
+      <FadeInSection>
+        <StaticImage
+          src="../images/obc-working.png"
+          alt="Engineers Working"
+          className="mb-8"
+        />
+      </FadeInSection>
+      <h2 className="text-2xl">Student Organization Building Satellites</h2>
+      <p>
+        Orbit is run with both technical and non-technical support from our
+        sponsors. We are very grateful for the support we receive, and are
+        always looking for new companies to work with. Want to be a part of our
+        journey? Send an email to cmo@orbitntnu.com!
+      </p>
+      <FadeInSection>
+        <StaticImage
+          src="../images/obc-working.png"
+          alt="Engineers Working"
+          className="my-8"
+        />
+      </FadeInSection>
+      <h2 className="text-2xl">Student Organization Building Satellites</h2>
+      <p>
+        Orbit is run with both technical and non-technical support from our
+        sponsors. We are very grateful for the support we receive, and are
+        always looking for new companies to work with. Want to be a part of our
+        journey? Send an email to cmo@orbitntnu.com!
+      </p>
+      <FadeInSection>
+        <StaticImage src="../images/framsat.png" alt="FramSat" />
+      </FadeInSection>
+    </section>
   </Layout>
 );
 
