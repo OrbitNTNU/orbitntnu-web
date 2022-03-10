9fdc15c053695e8e59a7d6d0927e8c52d9d61711
Isak Solheim <42111513+isaksolheim@users.noreply.github.com>
2022-03-10 15:42:56 +0100

    Use sanity data in nav and footer (#22)
    * Use data from sanity in footer
    * Use sanity data in navbar

---
+++ frontend/src/pages/selfiesat.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Header } from "../components/Header";
+import { Layout } from "../templates/Layout";
+
+const SelfieSat = () => (
+  <Layout>
+    <Header
+      title="Orbit NTNU"
+      name="SELFIESAT-1"
+      text="Orbit is run with both technical and non-technical support from our sponsors. 
+      We are very grateful for the support we receive, and are always looking for new companies 
+      to work with. Want to be a part of our journey? Send an email to cmo@orbitntnu.com!"
+    />
+  </Layout>
+);
+
+export default SelfieSat;
---
+++ sanity/schemas/textLink.ts
@@ -0,0 +1,17 @@
+export default {
+  name: "textLink",
+  type: "document",
+  title: "Text Link",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Title",
+    },
+    {
+      name: "url",
+      type: "string",
+      title: "Url",
+    },
+  ],
+};
