9f36149397073f10276966745cd41db3a2f5dff0
Isak Solheim <42111513+isaksolheim@users.noreply.github.com>
2022-03-15 20:14:49 +0100

    Disable locomotive scroll on safari (#27)

--- frontend/src/components/LocomotiveScroll/index.ts
+++ frontend/src/components/LocomotiveScroll/index.ts
@@ -1,25 +1,28 @@
 import { useEffect } from "react";
 import LocomotiveScroll from "locomotive-scroll";
 import { scroll } from "../../utils/scroll";
+import { isSafari } from "react-device-detect";
 
 export const Scroll = (callbacks) => {
   useEffect(() => {
-    let locomotiveScroll;
-    locomotiveScroll = new LocomotiveScroll({
-      el: document.querySelector(scroll.container),
-      ...scroll.options,
-    });
-    locomotiveScroll.update();
+    if (!isSafari) {
+      let locomotiveScroll;
+      locomotiveScroll = new LocomotiveScroll({
+        el: document.querySelector(scroll.container),
+        ...scroll.options,
+      });
+      locomotiveScroll.update();
 
-    window.scroll = locomotiveScroll;
+      window.scroll = locomotiveScroll;
 
-    locomotiveScroll.on("scroll", (func) => {
-      document.documentElement.setAttribute("data-direction", func.direction);
-    });
+      locomotiveScroll.on("scroll", (func) => {
+        document.documentElement.setAttribute("data-direction", func.direction);
+      });
 
-    return () => {
-      if (locomotiveScroll) locomotiveScroll.destroy();
-    };
+      return () => {
+        if (locomotiveScroll) locomotiveScroll.destroy();
+      };
+    }
   }, [callbacks]);
 
   return null;
