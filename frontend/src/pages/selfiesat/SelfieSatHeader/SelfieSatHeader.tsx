ffcb1c62a782217332b085bbed5d0ee6237d8ed1
isaksolheim <isak.m.h.s@gmail.com>
2022-03-18 11:42:58 +0100

    Refactor SelfieSatHeader

--- frontend/src/pages/selfiesat/SelfieSatHeader/SelfieSatHeader.tsx
+++ frontend/src/pages/selfiesat/SelfieSatHeader/SelfieSatHeader.tsx
@@ -1,34 +1,13 @@
 import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
 import Countdown from "react-countdown";
+import { CountdownRenderer } from "../../../components/CountdownRenderer";
 
 interface SelfieSatHeader {
   title: string;
   name: string;
   text: string;
 }
-
-const CountdownBox = ({ num, type }: { num: number; type: string }) => (
-  <div className="mr-4 bg-gray-800 bg-opacity-60 flex flex-col text-center w-16 h-16 md:w-24 md:h-24 justify-center">
-    <span className="text-2xl mt-1 md:text-4xl">{num}</span>
-    <span className="font-thin">{type}</span>
-  </div>
-);
-
-const renderer = ({ days, hours, minutes, seconds, completed }) => {
-  if (completed) {
-    return null;
-  } else {
-    return (
-      <div className="flex mt-4">
-        <CountdownBox num={days} type="Days" />
-        <CountdownBox num={hours} type="Hours" />
-        <CountdownBox num={minutes} type="Minutes" />
-        <CountdownBox num={seconds} type="Seconds" />
-      </div>
-    );
-  }
-};
 
 export const SelfieSatHeader = ({ title, name, text }: SelfieSatHeader) => {
   const launchDate = new Date(2022, 6, 1, 10);
@@ -47,7 +26,7 @@ export const SelfieSatHeader = ({ title, name, text }: SelfieSatHeader) => {
         <h2 className="font-bold text-4xl uppercase z-10">{name}</h2>
         <p className="max-w-64 ml-1 z-10">{text}</p>
         <div className="z-10">
-          <Countdown date={launchDate} renderer={renderer} />
+          <Countdown date={launchDate} renderer={CountdownRenderer} />
           <p className="ml-1 mt-2 font-thin md:font-normal">Until launch</p>
         </div>
       </div>
