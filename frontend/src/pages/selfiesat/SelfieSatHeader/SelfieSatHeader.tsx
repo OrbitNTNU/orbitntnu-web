996aacffa7ddb0ec3ef0dba1515913abbeff0fdd
isaksolheim <isak.m.h.s@gmail.com>
2022-03-18 11:33:01 +0100

    Add mobile image

--- frontend/src/pages/selfiesat/SelfieSatHeader/SelfieSatHeader.tsx
+++ frontend/src/pages/selfiesat/SelfieSatHeader/SelfieSatHeader.tsx
@@ -41,15 +41,20 @@ export const SelfieSatHeader = ({ title, name, text }: SelfieSatHeader) => {
         className="w-full h-96 relative opacity-70 lg:w-auto md:h-[28rem]"
       />
       <div className="flex flex-col absolute top-20 left-6 lg:top-1/4 lg:left-64">
-        <h1 className="text-gray-300 font-thin text-2xl uppercase -mb-2">
+        <h1 className="text-gray-300 font-thin text-2xl uppercase -mb-2 z-10">
           {title}
         </h1>
-        <h2 className="font-bold text-4xl uppercase ">{name}</h2>
-        <p className="max-w-64 ml-1">{text}</p>
-        <div>
+        <h2 className="font-bold text-4xl uppercase z-10">{name}</h2>
+        <p className="max-w-64 ml-1 z-10">{text}</p>
+        <div className="z-10">
           <Countdown date={launchDate} renderer={renderer} />
           <p className="ml-1 mt-2 font-thin md:font-normal">Until launch</p>
         </div>
+        <StaticImage
+          src="../../../images/selfiesat.png"
+          alt="Selfiesast"
+          className="absolute z-0"
+        />
       </div>
     </header>
   );
