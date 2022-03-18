86bdb8d6929a7fa94e0a5fb7d553409f1b75f5bd
isaksolheim <isak.m.h.s@gmail.com>
2022-03-18 11:09:21 +0100

    Add mobile countdown

--- frontend/src/pages/selfiesat/SelfieSatHeader/SelfieSatHeader.tsx
+++ frontend/src/pages/selfiesat/SelfieSatHeader/SelfieSatHeader.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
+import Countdown from "react-countdown";
 
 interface SelfieSatHeader {
   title: string;
@@ -7,19 +8,49 @@ interface SelfieSatHeader {
   text: string;
 }
 
-export const SelfieSatHeader = ({ title, name, text }: SelfieSatHeader) => (
-  <header className="relative -mb-8" data-scroll-section>
-    <StaticImage
-      src="../../../images/stars.png"
-      alt="Stars"
-      className="w-full h-96 relative opacity-70 lg:w-auto md:h-[28rem]"
-    />
-    <div className="flex flex-col absolute top-20 left-6 lg:top-1/4 lg:left-64">
-      <h1 className="text-gray-300 font-thin text-2xl uppercase -mb-2">
-        {title}
-      </h1>
-      <h2 className="font-bold text-4xl uppercase ">{name}</h2>
-      <p className="font-base max-w-64 ml-1">{text}</p>
-    </div>
-  </header>
+const CountdownBox = ({ num, type }: { num: number; type: string }) => (
+  <div className="mr-4 bg-gray-800 bg-opacity-60 flex flex-col text-center w-16 h-16">
+    <span className="text-2xl mt-1">{num}</span>
+    <span className="font-thin">{type}</span>
+  </div>
 );
+
+const renderer = ({ days, hours, minutes, seconds, completed }) => {
+  if (completed) {
+    return null;
+  } else {
+    return (
+      <div className="flex mt-4">
+        <CountdownBox num={days} type="Days" />
+        <CountdownBox num={hours} type="Hours" />
+        <CountdownBox num={minutes} type="Minutes" />
+        <CountdownBox num={seconds} type="Seconds" />
+      </div>
+    );
+  }
+};
+
+export const SelfieSatHeader = ({ title, name, text }: SelfieSatHeader) => {
+  const launchDate = new Date(2022, 6, 1, 10);
+
+  return (
+    <header className="relative -mb-8" data-scroll-section>
+      <StaticImage
+        src="../../../images/stars.png"
+        alt="Stars"
+        className="w-full h-96 relative opacity-70 lg:w-auto md:h-[28rem]"
+      />
+      <div className="flex flex-col absolute top-20 left-6 lg:top-1/4 lg:left-64">
+        <h1 className="text-gray-300 font-thin text-2xl uppercase -mb-2">
+          {title}
+        </h1>
+        <h2 className="font-bold text-4xl uppercase ">{name}</h2>
+        <p className="font-base max-w-64 ml-1">{text}</p>
+        <div>
+          <Countdown date={launchDate} renderer={renderer} />
+          <p className="ml-1 mt-2 font-thin">Until launch</p>
+        </div>
+      </div>
+    </header>
+  );
+};
