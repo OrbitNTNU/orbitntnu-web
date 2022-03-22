9427508470afe7da266fda37d0cc5e5e861c3d5b
isaksolheim <isak.m.h.s@gmail.com>
2022-03-22 13:08:58 +0100

    Add component style

--- frontend/src/components/ProfileCard/index.tsx
+++ frontend/src/components/ProfileCard/index.tsx
@@ -1,3 +1,4 @@
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 
 interface ProfileCardProps {
@@ -7,11 +8,21 @@ interface ProfileCardProps {
   phonenumber: number;
 }
 
-export const ProfileCard = ({ title, name, email, phonenumber }: ProfileCardProps) => (
-    <figure className="bg-blue-600 font-bold py-2 px-4 w-full md:max-w-64">
-        <p>{title}</p>
-        <p>{name}</p>
-        <p>{email}</p>
-        <p>{phonenumber}</p>
-    </figure>
+export const ProfileCard = ({
+  title,
+  name,
+  email,
+  phonenumber,
+}: ProfileCardProps) => (
+  <figure className="bg-gray-900 w-48 p-4 flex flex-col">
+    <StaticImage
+      src="../../images/ceo.png"
+      alt="Small Logo"
+      className="w-40 h-40 rounded-full m-auto mb-2"
+    />
+    <p className="text-orange-500 -mb-2">{title}</p>
+    <p className="text-xl">{name}</p>
+    <p className="font-thin">{email}</p>
+    <p className="font-thin">{phonenumber}</p>
+  </figure>
 );
