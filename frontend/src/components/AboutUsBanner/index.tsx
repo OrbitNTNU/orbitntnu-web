344320a23c485b1715d7e1003d97c7b72562a29e
Isak Solheim <42111513+isaksolheim@users.noreply.github.com>
2022-03-25 15:49:06 +0100

    Fix landing hero load position (#35)

--- frontend/src/components/AboutUsBanner/index.tsx
+++ frontend/src/components/AboutUsBanner/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { GatsbyImage, IGatsbyImageData } from "gatsby-plugin-image";
 import { Button } from "../Button";
 import { Link } from "gatsby";
+import { FadeInSection } from "../FadeInSection";
 
 interface AboutUsBannerProps {
   title: string;
@@ -21,13 +22,15 @@ export const AboutUsBanner = ({
   image,
 }: AboutUsBannerProps) => (
   <section className="relative flex justify-center" data-scroll-section>
-    <GatsbyImage
-      image={image.asset.gatsbyImageData}
-      alt="earth black backdrop"
-      className="w-full md:max-w-3xl"
-      data-scroll
-      data-scroll-speed="2"
-    />
+    <FadeInSection>
+      <GatsbyImage
+        image={image.asset.gatsbyImageData}
+        alt="earth black backdrop"
+        className="w-full md:max-w-3xl"
+        data-scroll
+        data-scroll-speed="2"
+      />
+    </FadeInSection>
     <div className="absolute top-1/2 -mt-32 max-w-64 text-center md:max-w-xl">
       <h2 className="text-4xl font-bold md:text-6xl">{title}</h2>
       <p className="mb-2 md:text-xl">{aboutText}</p>
