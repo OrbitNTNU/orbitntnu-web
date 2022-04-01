398d1789b571cc0c305fc2473d0cbba17240c118
isaksolheim <isak.m.h.s@gmail.com>
2022-04-01 20:21:07 +0200

    Fix styling

--- frontend/src/components/JoinCard/index.tsx
+++ frontend/src/components/JoinCard/index.tsx
@@ -9,13 +9,15 @@ interface JoinCardProps {
 }
 
 export const JoinCard = ({ title, text, applyLink }: JoinCardProps) => (
-  <div className="bg-gray-900 p-4 m-4 md:max-w-md">
+  <div className="bg-gray-900 p-4 m-4 md:max-w-md relative md:pb-16">
     <StaticImage
       src="../../images/obc-working.png"
       alt="Engineers working hard"
     />
     <h2 className="mt-2 text-2xl">{title}</h2>
     <p className="mb-4">{text}</p>
-    <ButtonLink label="Apply" url={applyLink} />
+    <div className="md:absolute md:bottom-4 md:left-4">
+      <ButtonLink label="Apply" url={applyLink} />
+    </div>
   </div>
 );
