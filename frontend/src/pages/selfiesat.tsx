3a2c70ebbdf7745a46d6a8550869fe606740173a
isaksolheim <isak.m.h.s@gmail.com>
2022-05-05 18:24:38 +0200

    Add SelfieSat image

--- frontend/src/pages/selfiesat.tsx
+++ frontend/src/pages/selfiesat.tsx
@@ -5,6 +5,7 @@ import { SelfieSatHeader } from "../views/selfiesat/SelfieSatHeader";
 import { Specs } from "../views/selfiesat/Specs";
 import { FadeInSection } from "../components/FadeInSection";
 import firebase from "gatsby-plugin-firebase";
+import { StaticImage } from "gatsby-plugin-image";
 
 const SelfieSat = () => {
   useEffect(() => {
@@ -28,6 +29,13 @@ const SelfieSat = () => {
       <FadeInSection>
         <Specs />
       </FadeInSection>
+      <div className="flex justify-center mt-4">
+        <StaticImage
+          src="../images/selfiesat-image.JPG"
+          alt="SelfieSat"
+          className="max-w-screen-lg mx-8"
+        />
+      </div>
     </Layout>
   );
 };
