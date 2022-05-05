18b8e74d961af633b86b40a6bf4e8762e7d35a2b
isaksolheim <isak.m.h.s@gmail.com>
2022-05-05 18:38:08 +0200

    Move About Us Page to index

--- frontend/src/components/AboutUsBanner/index.tsx
+++ frontend/src/components/AboutUsBanner/index.tsx
@@ -13,6 +13,7 @@ interface AboutUsBannerProps {
       gatsbyImageData: IGatsbyImageData;
     };
   };
+  executeScroll: () => any;
 }
 
 export const AboutUsBanner = ({
@@ -20,6 +21,7 @@ export const AboutUsBanner = ({
   aboutText,
   buttonText,
   image,
+  executeScroll,
 }: AboutUsBannerProps) => (
   <section className="relative flex justify-center">
     <FadeInSection>
@@ -32,9 +34,7 @@ export const AboutUsBanner = ({
     <div className="absolute top-1/2 -mt-32 max-w-64 text-center md:max-w-xl">
       <h2 className="text-4xl font-bold md:text-6xl mb-2">{title}</h2>
       <p className="mb-4 md:text-xl">{aboutText}</p>
-      <Link to="/about">
-        <Button label={buttonText} />
-      </Link>
+      <Button label={buttonText} onClick={executeScroll} />
     </div>
   </section>
 );
