491196ef8109f36ca9f27678e03ac023c5e895e5
isaksolheim <isak.m.h.s@gmail.com>
2022-05-06 10:03:34 +0200

    Sort teams filter

--- frontend/src/views/teams/Filter/index.tsx
+++ frontend/src/views/teams/Filter/index.tsx
@@ -11,21 +11,26 @@ export const Filter = ({
   teams,
   selectedTeam,
   setSelectedTeam,
-}: FilterProps) => (
-  <ul className="flex flex-wrap justify-center gap-4">
-    {teams.map((team) => {
-      const selected = team.name === selectedTeam.name;
-      return (
-        <li
-          key={team.name}
-          className={`cursor-pointer text-lg ${
-            selected ? "text-yellow-500" : "text-white"
-          }`}
-          onClick={() => setSelectedTeam(team)}
-        >
-          {team.name}
-        </li>
-      );
-    })}
-  </ul>
-);
+}: FilterProps) => {
+  teams.sort((a, b) => (a.name < b.name ? 1 : -1));
+  teams.sort((a, b) => (a.name === "The Board" ? -1 : 1));
+
+  return (
+    <ul className="flex flex-wrap justify-center gap-4">
+      {teams.map((team) => {
+        const selected = team.name === selectedTeam.name;
+        return (
+          <li
+            key={team.name}
+            className={`cursor-pointer text-lg ${
+              selected ? "text-yellow-500" : "text-white"
+            }`}
+            onClick={() => setSelectedTeam(team)}
+          >
+            {team.name}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
