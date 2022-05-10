95dc1194daa321bfb6ccfe4d190dc0436d173c23
Isak Solheim <42111513+isaksolheim@users.noreply.github.com>
2022-05-10 10:46:18 +0200

    Make The Board the default selected team (#85)

--- frontend/src/views/teams/Filter/index.tsx
+++ frontend/src/views/teams/Filter/index.tsx
@@ -11,26 +11,21 @@ export const Filter = ({
   teams,
   selectedTeam,
   setSelectedTeam,
-}: FilterProps) => {
-  teams.sort((a, b) => (a.name > b.name ? 1 : -1));
-  teams.sort((a, b) => (a.name === "The Board" ? -1 : 1));
-
-  return (
-    <ul className="flex flex-wrap justify-center">
-      {teams.map((team) => {
-        const selected = team.name === selectedTeam.name;
-        return (
-          <li
-            key={team.name}
-            className={`cursor-pointer text-lg mx-2 ${
-              selected ? "text-yellow-500" : "text-white"
-            }`}
-            onClick={() => setSelectedTeam(team)}
-          >
-            {team.name}
-          </li>
-        );
-      })}
-    </ul>
-  );
-};
+}: FilterProps) => (
+  <ul className="flex flex-wrap justify-center">
+    {teams.map((team) => {
+      const selected = team.name === selectedTeam.name;
+      return (
+        <li
+          key={team.name}
+          className={`cursor-pointer text-lg mx-2 ${
+            selected ? "text-yellow-500" : "text-white"
+          }`}
+          onClick={() => setSelectedTeam(team)}
+        >
+          {team.name}
+        </li>
+      );
+    })}
+  </ul>
+);
