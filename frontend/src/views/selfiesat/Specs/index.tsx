aaab70d516e000ec503ba68bb72153254c572b66
isaksolheim <isak.m.h.s@gmail.com>
2022-06-01 20:23:52 +0200

    Extend selfiesat specifications

--- frontend/src/views/selfiesat/Specs/index.tsx
+++ frontend/src/views/selfiesat/Specs/index.tsx
@@ -1,9 +1,5 @@
-import {
-  GatsbyImage,
-  IGatsbyImageData,
-  StaticImage,
-} from "gatsby-plugin-image";
 import React from "react";
+import { GatsbyImage, IGatsbyImageData } from "gatsby-plugin-image";
 
 interface SpecsProps {
   specs: {
@@ -16,7 +12,7 @@ interface SpecsProps {
 export const Specs = ({ specs, image }: SpecsProps) => (
   <div className="mx-8 md:max-w-4xl md:m-auto">
     <h3 className="text-gray-300 text-center md:text-left">SELFIESAT-1</h3>
-    <h2 className="text-3xl font-bold text-center md:text-left md:-mb-4">
+    <h2 className="text-3xl font-bold text-center md:text-left">
       SPECIFICATIONS
     </h2>
     <div className="flex flex-col justify-center md:flex-row-reverse">
@@ -38,5 +34,10 @@ export const Specs = ({ specs, image }: SpecsProps) => (
         ))}
       </ul>
     </div>
+    <p className="mt-8 text-center">
+      The beacons contain information about the electrical power systems, and
+      the string <pre className="text-sm inline">SelfieSat</pre> repeated 20
+      times.
+    </p>
   </div>
 );
