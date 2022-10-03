b781655c4f92d6e2cd7a7281865c2473dd2b2b3f
Isak Solheim <42111513+isaksolheim@users.noreply.github.com>
2022-10-03 15:38:07 +0200

    Fix team member clone bug (#115)

--- frontend/src/views/teams/TeamsContainer/index.tsx
+++ frontend/src/views/teams/TeamsContainer/index.tsx
@@ -14,13 +14,13 @@ export const TeamsContainer = ({
   selectedTeam,
   setSelectedTeam,
 }: TeamsContainerProps) => (
-  <section className="mt-16 px-8 relative md:flex md:flex-col md:max-w-4xl md:justify-center m-auto">
+  <section className="mt-16 px-8 relative md:flex md:flex-col md:max-w-5xl md:justify-center m-auto">
     <Filter
       teams={teams}
       selectedTeam={selectedTeam}
       setSelectedTeam={setSelectedTeam}
     />
-    <p className="p-4 my-4 border-t border-b border-yellow-500">
+    <p className="p-4 my-4 border-t border-b border-yellow-500 md:max-w-4xl md:mx-auto">
       {selectedTeam.description}
     </p>
     <Members members={selectedTeam.members} />
