19f1096891aa6db9350082121bd3f0d25afb7d45
Isak Solheim <42111513+isaksolheim@users.noreply.github.com>
2022-10-21 12:24:19 +0200

    Add biosat page (#117)

--- sanity/schemas/nextSatPage.ts
+++ sanity/schemas/nextSatPage.ts
@@ -9,21 +9,34 @@ export default {
       title: "Title",
     },
     {
-      name: "fadedTitle",
-      type: "string",
-      title: "Faded Title",
-      description: "Faded text on top of title",
-    },
-    {
       name: "topText",
       type: "string",
       title: "Top Text",
     },
     {
-      name: "topImage",
-      title: "Top Image",
+      name: "shortTopText",
+      type: "string",
+      title: "Short Top Text",
+    },
+    {
+      name: "launchDate",
+      type: "datetime",
+      title: "Launch Date",
+    },
+    {
+      name: "specifications",
+      title: "Specifications",
+      type: "array",
+      of: [{ type: "specification" }],
+    },
+    {
+      name: "specificationsImage",
+      title: "Image",
       type: "image",
-      description: "Top Image Shown On Page",
+      description: "Image to the right of the specs",
+      options: {
+        hotspot: true,
+      },
     },
     {
       name: "firstSectionTitle",
@@ -31,8 +44,13 @@ export default {
       type: "string",
     },
     {
-      name: "firstSectionText",
-      title: "First Section Text",
+      name: "firstSectionText1",
+      title: "First Section Text1",
+      type: "string",
+    },
+    {
+      name: "firstSectionText2",
+      title: "First Section Text2",
       type: "string",
     },
     {
@@ -49,6 +67,72 @@ export default {
       title: "Image",
       type: "image",
       description: "Image at the bottom of section 1",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "secondSectionTitle",
+      title: "Second Section Title",
+      type: "string",
+    },
+    {
+      name: "secondSectionText",
+      title: "Second Section Text",
+      type: "string",
+    },
+    {
+      name: "secondSectionImage",
+      title: "Image",
+      type: "image",
+      description: "Image to the right of section 2",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "thirdSectionTitle",
+      title: "Third Section Title",
+      type: "string",
+    },
+    {
+      name: "thirdSectionText",
+      title: "Third Section Text",
+      type: "string",
+    },
+    {
+      name: "thirdSectionImage",
+      title: "Image",
+      type: "image",
+      description: "Image to the left of section 3",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "fourthSectionTitle",
+      title: "Fourth Section Title",
+      type: "string",
+    },
+    {
+      name: "fourthSectionText",
+      title: "Fourth Section Text",
+      type: "string",
+    },
+    {
+      name: "fourthSectionImage",
+      title: "Image",
+      type: "image",
+      description: "Image to the right of section 4",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "bottomImage",
+      title: "Image",
+      type: "image",
+      description: "Image at the bottom of the page",
       options: {
         hotspot: true,
       },
