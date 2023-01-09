3c337e2cb736201bc16f4b865ff01f0cb6274bde
Isak Solheim <42111513+isaksolheim@users.noreply.github.com>
2023-01-09 18:19:50 +0100

    Update button hover state (#121)

--- frontend/src/components/ButtonLink/index.tsx
+++ frontend/src/components/ButtonLink/index.tsx
@@ -8,7 +8,7 @@ interface ButtonLinkProps {
 export const ButtonLink = ({ label, url }: ButtonLinkProps) => (
   <a
     href={url}
-    className="bg-blue-600 font-bold py-2 px-4 w-32 text-center md:text-xl block"
+    className="bg-blue-600 font-bold py-2 px-4 w-32 text-center md:text-xl block hover:bg-blue-800"
   >
     {label}
   </a>
