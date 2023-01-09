619eba87907a329aa0b7e5b2774a252a10d523ca
isaksolheim <mail@isaks.io>
2023-01-09 18:19:24 +0100

    Update button hover state

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 
 export const Button = ({ label, onClick }: ButtonProps) => (
   <button
-    className="bg-blue-600 font-bold py-2 px-4 w-full md:max-w-64"
+    className="bg-blue-600 font-bold py-2 px-4 w-full md:max-w-64 hover:bg-blue-800"
     onClick={onClick}
   >
     {label}
