8314c0d68b52d3ad8ad36bf6e2a4a48a9cf77573
simonbjerkas <simon.bjerkas10@gmail.com>
2023-03-01 18:17:30 +0100

    Suborbital added to sanity

--- frontend/src/pages/suborbital.tsx
+++ frontend/src/pages/suborbital.tsx
@@ -1,10 +1,12 @@
+import { graphql } from "gatsby";
+import firebase from "gatsby-plugin-firebase";
+import { GatsbyImage } from "gatsby-plugin-image";
 import React, { useEffect } from "react";
+import { BlogLink } from "../components/BlogLink";
+import { FadeInSection } from "../components/FadeInSection";
 import { Header } from "../components/Header";
 import { Layout } from "../templates/Layout";
-import firebase from "gatsby-plugin-firebase";
-import { graphql } from "gatsby";
 import { MissionText } from "../views/suborbital/MissionText";
-import { BlogLink } from "../components/BlogLink";
 
 const SubOrbital = ({ data }) => {
   const { sanitySuborbitalPage } = data;
@@ -25,7 +27,53 @@ const SubOrbital = ({ data }) => {
         text={sanitySuborbitalPage.topText}
         image={sanitySuborbitalPage.topImage}
       />
-      <MissionText />
+      <section className="mt-24 px-8 relative md:flex md:flex-col md:max-w-4xl md:justify-center m-auto">
+        <div className="md:flex md:gap-8 md:basis-0">
+          <div>
+            <h2 className="text-2xl md:text-4xl">
+              {sanitySuborbitalPage.firstSectionTitle}
+            </h2>
+            <p className="md:text-base">
+              {sanitySuborbitalPage.firstSectionText}
+            </p>
+          </div>
+
+          <FadeInSection>
+            <GatsbyImage
+              image={
+                sanitySuborbitalPage.firstSectionImage.asset.gatsbyImageData
+              }
+              alt="Image 1"
+              className="mt-2 mb-8 md:mt-0 md:mb-0"
+            />
+          </FadeInSection>
+        </div>
+
+        <FadeInSection>
+          <GatsbyImage
+            image={sanitySuborbitalPage.midImage.asset.gatsbyImageData}
+            alt="Image 2"
+            className="mb-8 md:my-8 md:mb-0"
+          />
+        </FadeInSection>
+
+        <div className="md:flex md:flex-row-reverse md:gap-8 md:my-8">
+          <div className="md:text-base md:min-w-64">
+            <p>{sanitySuborbitalPage.secondSectionText1}</p>
+            <p>{sanitySuborbitalPage.secondSectionText2}</p>
+          </div>
+
+          <FadeInSection>
+            <GatsbyImage
+              image={
+                sanitySuborbitalPage.secondSectionImage.asset.gatsbyImageData
+              }
+              alt="Image 3"
+              className="mt-2 md:mt-0"
+            />
+          </FadeInSection>
+        </div>
+      </section>
 
       {/* <div className="flex gap-8 flex-wrap bg-white p-16">
         <BlogLink linkUrl="/selfiesat" />
@@ -45,6 +93,25 @@ export const query = graphql`
       }
       title
       fadedTitle
+      firstSectionTitle
+      firstSectionText
+      firstSectionImage {
+        asset {
+          gatsbyImageData(fit: FILLMAX, placeholder: BLURRED)
+        }
+      }
+      midImage {
+        asset {
+          gatsbyImageData(fit: FILLMAX, placeholder: BLURRED)
+        }
+      }
+      secondSectionText1
+      secondSectionText2
+      secondSectionImage {
+        asset {
+          gatsbyImageData(fit: FILLMAX, placeholder: BLURRED)
+        }
+      }
     }
   }
 `;
