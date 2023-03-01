2f90f202ee4e5109b5d4848bde604a80d2e9cb6f
Simon Bjerkås <simon.bjerkas10@gmail.com>
2023-03-01 18:31:17 +0100

    Suborbital added to sanity (#131)

--- sanity/schemas/suborbitalPage.ts
+++ sanity/schemas/suborbitalPage.ts
@@ -25,5 +25,42 @@ export default {
       type: "image",
       description: "Top Image Shown On Page",
     },
+    {
+      name: "firstSectionTitle",
+      title: "First Section Title",
+      type: "string",
+    },
+    {
+      name: "firstSectionText",
+      title: "First Section Text",
+      type: "text",
+    },
+    {
+      name: "firstSectionImage",
+      title: "First Section Image",
+      type: "image",
+      description: "Image shown to the right of section text",
+    },
+    {
+      name: "midImage",
+      title: "Image",
+      type: "image",
+    },
+    {
+      name: "secondSectionText1",
+      title: "Second Section Text 1",
+      type: "text",
+    },
+    {
+      name: "secondSectionText2",
+      title: "Second Section Text 2",
+      type: "text",
+    },
+    {
+      name: "secondSectionImage",
+      title: "Second Section Image",
+      type: "image",
+      description: "Image shown to the left of section text",
+    },
   ],
 };
