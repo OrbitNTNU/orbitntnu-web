07e82eef1d8c915bf2a59967bb9ab9b1fbeb35b7
Hannmhaa <124441966+Hannmhaa@users.noreply.github.com>
2023-03-21 11:22:30 +0100

    Add hover and currentPath-border to navbar, teams and footer (#132)
    Co-authored-by: isaksolheim <mail@isaks.io>

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -5,6 +5,7 @@ import { FaBars } from "@react-icons/all-files/fa/FaBars";
 import { Link } from "gatsby";
 import { CSSTransition } from "react-transition-group";
 import "./navbar.css";
+import { isBrowser } from "../../utils/isBrowser";
 
 export const Navbar = () => {
   const [toggle, setToggle] = useState(false);
@@ -24,6 +25,10 @@ export const Navbar = () => {
     { name: "SUBORBITAL", url: "/suborbital" },
   ];
 
+  const getSelectedStatus = (url: string) => {
+    return isBrowser() && window.location.pathname === url ? "border-b-2" : "";
+  };
+
   return (
     <nav className="text-white p-2 mb-2  w-full z-50 absolute top-0 left-0">
       <Link to="/">
@@ -38,13 +43,24 @@ export const Navbar = () => {
       <ul className="invisible inline-block mt-5 ml-8 md:visible">
         {links.map((link) => (
           <Link key={link.name} to={link.url}>
-            <li className="inline-block mr-4">{link.name}</li>
+            <li
+              className={`inline-block mr-4 hover:border-b-2 hover:border-yellow-500 ${getSelectedStatus(
+                link.url
+              )}`}
+            >
+              {link.name}
+            </li>
           </Link>
         ))}
       </ul>
 
-      <Link to="/join" className="invisible absolute right-16 top-7 md:visible">
-        Join
+      <Link
+        to="/join"
+        className={`invisible absolute right-16 top-7 md:visible hover:border-b-2 hover:border-yellow-500 ${getSelectedStatus(
+          "/join"
+        )}`}
+      >
+        JOIN
       </Link>
 
       <FaBars
