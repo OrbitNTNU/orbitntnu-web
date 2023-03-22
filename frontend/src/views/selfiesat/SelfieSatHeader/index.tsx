0a53892a1c06a8c190a2ba7dda2a39e97c8f565b
Adelina Ghindaoanu <nadineag@stud.ntnu.no>
2023-03-22 19:44:33 +0100

    Update selfiesat header

--- frontend/src/views/selfiesat/SelfieSatHeader/index.tsx
+++ frontend/src/views/selfiesat/SelfieSatHeader/index.tsx
@@ -9,11 +9,11 @@ interface SelfieSatHeader {
 
 export const SelfieSatHeader = ({ title, name, text }: SelfieSatHeader) => (
   <header className="relative -mb-8">
-    <div className="relative h-[24rem] bg-black md:h-[28rem]">
-    <StaticImage
-      src="../../../images/16_selfie.png" 
-      alt="Stars"
-      className={`w-full h-full absolute opacity-90`}/>
+    <div className="flex justify-center relative h-[24rem] bg-black md:h-[28rem]">
+      <StaticImage
+        src="../../../images/16_selfie.png"
+        alt="Stars"
+        className={`max-w-4xl h-full absolute opacity-90`} />
     </div>
     <div className="flex flex-col absolute top-20 left-6 lg:top-1/4 lg:left-64">
       <h1 className="text-gray-300 font-thin text-2xl uppercase -mb-2 z-10">
