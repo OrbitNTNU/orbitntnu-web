c3e74abf1eb7cd105bb9650ce51aeb03c7e6b22e
Hanne Marie Haakaas <hanne.haakaas@gmail.com>
2023-04-27 13:16:59 +0200

    Updated framsat specs

--- frontend/src/views/framsat/Specs/index.tsx
+++ frontend/src/views/framsat/Specs/index.tsx
@@ -12,11 +12,9 @@ interface SpecsProps {
 }
 
 export const Specs = ({ name, specs, image }: SpecsProps) => (
-  <div className="mt-8 mx-8 md:max-w-2xl md:m-auto">
-    <h3 className="text-gray-300 text-center md:text-left md:mt-16">
-      {name}
-    </h3>
-    <h2 className="text-3xl font-bold text-center md:text-left md:-mb-8">
+  <div className="mt-8 md:max-w-3xl md:m-auto">
+    <h3 className="text-gray-300 text-center md:text-left">{name}</h3>
+    <h2 className="text-3xl font-bold text-center md:text-left">
       SPECIFICATIONS
     </h2>
     <div className="flex flex-col justify-center md:flex-row-reverse">
@@ -25,10 +23,10 @@ export const Specs = ({ name, specs, image }: SpecsProps) => (
         alt="Open Framsat"
         className="max-w-[270px] m-auto"
       />
-      <ul className="w-[270px] md:w-[300px] m-auto md:mr-28">
+      <ul className="w-[290px] md:w-[300px] m-auto md:mr-28">
         {specs.map((stat) => (
           <li
-            className="max-w-[270px] md:max-w-none mx-4 md:mx-0 h-8 mt-2 border-b border-yellow-500"
+            className="max-w-[290px] md:max-w-none mx-4 md:mx-0 h-8 mt-2 border-b border-yellow-500"
             key={stat.name}
           >
             <span className="float-left font-bold">{stat.name}</span>
