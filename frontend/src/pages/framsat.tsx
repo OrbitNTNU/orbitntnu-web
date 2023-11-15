75d5a2d58be7ee1bc089d478f634f6d16ca65c5b
Magnus Giverin <magnusgiverin@icloud.com>
2023-11-15 19:24:34 +0100

    Revert: Add framsat 2 page

--- frontend/src/pages/framsat.tsx
+++ frontend/src/pages/framsat.tsx
@@ -10,18 +10,10 @@ import { graphql } from "gatsby";
 import { Team } from "./teams";
 import { Members } from "../views/teams/Members";
 import { GatsbyImage, IGatsbyImageData } from "gatsby-plugin-image";
-import FramSatContainer, { FramSatContainerInfo } from "../views/framsat/FramSatContainer";
-import framsat2 from "../views/framsat/framsat2Info";
 
 const FramSat = ({ data }) => {
   const [selectedTeam, setSelectedTeam] = useState<Team>();
   const { sanityFramsatPage, allSanityTeam } = data;
-
-  const framsat1Info: FramSatContainerInfo = {
-    info: sanityFramsatPage,
-  }; 
-  const framsat2Info: FramSatContainerInfo = framsat2();
-
   useEffect(() => {
     if (!firebase) {
       return;
@@ -37,54 +29,64 @@ const FramSat = ({ data }) => {
 
   console.log(data);
 
-  const framsats: FramSatContainerInfo[] = [
-    framsat1Info,
-    framsat2Info
-  ];
-
-  const [selectedFramsat, setSelectedFramsat] = useState<FramSatContainerInfo>(framsat1Info);
-
   return (
     <Layout>
       <FramSatHeader
-        title="OrbitNTNU"
-        name="FramSat"
+        title={sanityFramsatPage.title}
+        name={sanityFramsatPage.topText}
         text={sanityFramsatPage.shortTopText}
       />
       <section className="mt-16 px-8 relative md:flex md:flex-col md:max-w-4xl md:justify-center m-auto">
         <FadeInSection>
           <div>
-            <div className="flex px-8 justify-center m-auto md:-mt-6">
-              <p className="max-w-4xl md:pt-6 mb-8 mt-2 md:mb-0">
-                {framsat2Info.info.missionTitle}
+            <h2 className="text-center text-4xl font-bold mb-4">
+              {sanityFramsatPage.missionTitle}
+            </h2>
+            <div className="flex flex-col px-8 items-center md:flex-row md:max-w-4xl m-auto gap-8 md:-mt-6">
+              <p className="max-w-sm md:pt-6 mb-4 md:mb-0">
+                {sanityFramsatPage.missionTextLeft}
+              </p>
+              <p className="max-w-sm md:pt-6 mb-4 md:mb-0">
+                {sanityFramsatPage.missionTextRight}
               </p>
             </div>
           </div>
         </FadeInSection>
       </section>
 
+      <FadeInSection>
+        <div className="mt-16 mb-8">
+          <Specs
+            name="FRAMSAT"
+            specs={sanityFramsatPage.specifications}
+            image={sanityFramsatPage.specificationsImage.asset.gatsbyImageData}
+          />
+        </div>
+        <p className="mx-8 md:max-w-2xl md:m-auto">
+          {sanityFramsatPage.firstSectionText}
+        </p>
+        <p className="mx-8 md:max-w-2xl md:m-auto">
+          You can find the budget 
+          <a 
+            href="/framsat-1-doc/framsat1_budget.pdf" 
+            target="_blank" rel="noopener noreferrer" 
+            className="text-[#1e90ff]" 
+            onMouseOver={e => e.target.style.color="#0e71d4"}
+            onMouseOut={e => e.target.style.color="#1e90ff"}
+          > here
+          </a>.
+        </p>
+      </FadeInSection>
 
-      <section className="flex justify-center mt-8">
-        <FadeInSection>
-          <div className="w-full flex mb-8 h-[2px] bg-gradient-to-r from-black via-blue-500 to-black"/>
-          <ul className="flex flex-row flex-wrap gap-8 md:gap-14 mx-2 justify-center md:max-w-4xl md:m-auto">
-          {framsats.map((satellite) => (
-            <li
-              key={satellite.info.title}
-              className={`cursor-pointer text-2xl font-bold hover:text-yellow-600 ${
-                selectedFramsat.info.title === satellite.info.title ?
-                "text-yellow-500"
-                :
-                false}`}
-              onClick={() => setSelectedFramsat(satellite)}>
-              {satellite.info.title}
-            </li>
-          ))}
-          </ul>
-          <div className="w-full my-8 h-[2px] bg-gradient-to-r from-black via-blue-500 to-black"/>
-          <FramSatContainer info={selectedFramsat.info}/>
-        </FadeInSection>
-      </section>
+      <FadeInSection>
+        <div className="flex justify-center mt-8">
+          <GatsbyImage
+            image= {sanityFramsatPage.firstSectionImage.asset.gatsbyImageData}
+            alt="Open Framsat"
+            className="max-w-screen-lg mx-8"
+          />
+        </div>
+      </FadeInSection>
 
       <section className="mt-6 px-8 relative md:flex md:flex-col md:max-w-4xl md:justify-center m-auto">
         <h2 className="text-center text-4xl font-bold mb-2">
@@ -97,7 +99,7 @@ const FramSat = ({ data }) => {
             </p>
             <Members
               members={selectedTeam.members.filter((member) =>
-                member.title.includes(selectedFramsat.info.title.substring(0, 9))
+                member.title.includes("FramSat")
               )}
               wide
             />
