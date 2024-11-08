2f2af7534cab415f98adc0270c71603a4e19ac4d
Magnus Giverin <magnusgiverin@icloud.com>
2024-11-08 14:24:14 +0100

    Fix: Fix erronious sorting on front end

--- frontend/src/views/teams/Members/index.tsx
+++ frontend/src/views/teams/Members/index.tsx
@@ -8,14 +8,9 @@ interface MembersProps {
 }
 
 export const Members = ({ members, wide = false }: MembersProps) => {
-  // Sort members based on title priority: CEO and COO first, NTNU_REP last
-  const sortedMembers = members.sort((a, b) => {
-    return a.title.localeCompare(b.title); // Alphabetical sort for others
-  });
-
   return (
     <ul className="flex flex-wrap justify-center">
-      {sortedMembers.map((member) => (
+      {members.map((member) => (
         <ProfileCard
           key={`${member.name}-${member.title}`}
           member={member}
